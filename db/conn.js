d46e94b815435c6f75972b83078f38943aa18787
mohit1607 <57750800+mohit1607@users.noreply.github.com>
2022-12-29 23:34:05 +0530

    Updated the Mongo URI
    This commit consist of a process.env.MongoURI which was set to a dummy uri

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connect = async() => {
-    await mongoose.connect( `mongodb+srv://<UserName>:<Password>@projectscluster.zntoqfe.mongodb.net/?retryWrites=true&w=majority`, {
+    await mongoose.connect( process.env.MONGO_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     }).then(() => console.log( 'connection Successfully setup')).catch((err) => { console.log(err)})
